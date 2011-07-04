d01313343cf2409f7f2449aa83908020f2c80193
Jeremy Tregunna <jeremy.tregunna@me.com>
2011-07-04 11:27:16 -0600

    fixes error with awk

--- git-simple-common.sh
+++ git-simple-common.sh
@@ -49,6 +49,6 @@ unstash_it() {
 
     CURRENT_BRANCH_NAME=`current_branch_name`
     warn "Unstashing branch '${CURRENT_BRANCH_NAME}'"
-    STASH_INDEX=`git stash list | grep ": On (" | awk -F'[{}]' '( ~/stash@$/{print [}'`
+    STASH_INDEX=`git stash list | grep ": On $1" | awk -F'[{}]' '$1 ~/stash@$/{print $2}'`
     [ -n ${STASH_INDEX} ] && git stash pop "stash@{${STASH_INDEX}}"
 }
